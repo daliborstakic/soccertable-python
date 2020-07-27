b21d4aa2d50d944cc900af07360ac774ac096f89
daliborstakic <stakicdalibor64@gmail.com>
2020-07-27 19:04:07 +0200

    Added two properties inside the Club() class.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 class Club(object):
     def __init__(self):
-        pass
+        self.gs = 0
+        self.ga = 0
+        self.wi = 0
+        self.dr = 0
+        self.lo = 0
+
+    @property
+    def goalDifference(self):
+        return self.gs - self.ga
+
+    @property
+    def points(self):
+        return self.wi * 3 + self.dr
